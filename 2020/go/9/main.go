85b8a282ecdbaa084e86ad33af2d3fcd7897cce6
Jason Stewart <jason@bendyworks.com>
2020-12-09 09:44:46 -0500

    Day 9, part 2

--- 2020/go/9/main.go
+++ 2020/go/9/main.go
@@ -42,13 +42,38 @@ func partOne(data []int) int {
 	return 0
 }
 
+func partTwo(target int, data []int) int {
+	for i := 0; i < len(data)-2; i++ {
+		sum, sm, lg := data[i], data[i], data[i]
+		for j := i + 1; j < len(data); j++ {
+			if data[j] < sm {
+				sm = data[j]
+			}
+			if data[j] > lg {
+				lg = data[j]
+			}
+
+			sum += data[j]
+			if sum == target {
+				return sm + lg
+
+			}
+		}
+	}
+	return 0
+}
+
 func main() {
 	data, err := ioutil.ReadFile("./input.txt")
-	lines := strings.Split(string(data), "\n")
+	lines := asInt(strings.Split(string(data), "\n"))
 
 	if err != nil {
 		log.Fatal("Error handling input")
 	}
 
-	fmt.Println("Part 1:", partOne(asInt(lines)))
+	p1 := partOne(lines)
+	p2 := partTwo(p1, lines)
+
+	fmt.Println("Part 1:", p1)
+	fmt.Println("Part 2:", p2)
 }
