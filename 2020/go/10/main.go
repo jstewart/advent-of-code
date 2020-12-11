4579fcb4c7092d64dc094bf5ce3faea00858e7aa
Jason Stewart <jstewart@fusionary.com>
2020-12-10 19:35:28 -0500

    Day 10, pt 2

--- 2020/go/10/main.go
+++ 2020/go/10/main.go
@@ -26,6 +26,48 @@ func partOne(input []int) int {
 	return (oneJolt * threeJolt)
 }
 
+func permutations(val int, set map[int]bool, cache map[int]int) int {
+	if cached, ok := cache[val]; ok {
+		return cached
+	}
+	if val == 0 {
+		cache[val] = 1
+		return 1
+	}
+	if val < 0 {
+		cache[val] = 0
+		return 0
+	}
+	if _, ok := set[val]; !ok {
+		cache[val] = 0
+		return 0
+	}
+	cache[val] = permutations(val-1, set, cache) +
+		permutations(val-2, set, cache) +
+		permutations(val-3, set, cache)
+
+	return cache[val]
+}
+
+func partTwo(input []int) int {
+	var max int
+	set := make(map[int]bool)
+
+	for _, x := range input {
+		if x > max {
+			max = x
+		}
+		set[x] = true
+	}
+
+	// Add terminal and device
+	max += 3
+	set[0] = true
+	set[max] = true
+
+	return permutations(max, set, make(map[int]int))
+}
+
 func main() {
 	data, err := ioutil.ReadFile("./input.txt")
 	lines := strings.Split(string(data), "\n")
@@ -40,4 +82,5 @@ func main() {
 		input = append(input, i)
 	}
 	fmt.Println("part 1:", partOne(input))
+	fmt.Println("part 2:", partTwo(input))
 }
